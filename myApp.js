a1d5d20d6b9ec3c0d16362b3fc4f956ab781953e
Micaela <micaela.sch.aquino@gmail.com>
2021-09-05 19:19:31 -0300

    subo

--- myApp.js
+++ myApp.js
@@ -1,37 +1,23 @@
 var express = require('express');
 var app = express();
- console.log("Hello World");
+console.log("Hello World");
 
+absolutePath = __dirname + '/views/index.html';
+publicPath = __dirname + '/public';
+// app.get('/', function (req, res) {
+//         res.send('Hello Express');    
+//     });
 
+app.get('/', function (req, res) {
+    res.sendFile(absolutePath);
+});
 
+app.use('/public', express.static(publicPath));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/json', function (req, res) {
+    res.json({ "message": "Hello json" })
+    
+})
 
 
 
